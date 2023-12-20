b839b1f3f52fb8e6666be78d402c90c1168e3823
alexharri <alexharri2919@gmail.com>
2023-12-20 20:21:14 +0000

    perf: add fastbitset to iterator benchmark

--- benchmarks/iterator/run.ts
+++ benchmarks/iterator/run.ts
@@ -1,18 +1,24 @@
 import fs from "fs";
 import path from "path";
-import { BitSet } from "../../src/bitset";
+import { BitSet as BitSet_alexharri } from "../../src/bitset";
 import { profile } from "../profile";
+import { makeFastBitSet } from "../utils";
 
 let bitstrings = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, "10k-bitstrings.json"), "utf-8")
 ) as string[];
-let bitsets = bitstrings.map((bitstring) => new BitSet(bitstring));
+let bitsets_alexharri = bitstrings.map(
+  (bitstring) => new BitSet_alexharri(bitstring)
+);
+let bitsets_fastbitset = bitstrings.map((bitstring) =>
+  makeFastBitSet(bitstring)
+);
 
 console.log("Running '10k-bitstrings' benchmark");
 profile(
   () => {
     for (let n = 0; n < 5; n++) {
-      for (const bitset of bitsets) {
+      for (const bitset of bitsets_alexharri) {
         let count = 0;
         for (const _ of bitset) {
           count++;
@@ -22,6 +28,19 @@ profile(
   },
   (timeMs) => console.log(`\t'alexharri/bitset' ran in ${timeMs.toFixed(1)} ms`)
 );
+profile(
+  () => {
+    for (let n = 0; n < 5; n++) {
+      for (const bitset of bitsets_fastbitset) {
+        let count = 0;
+        for (const _ of bitset as any) {
+          count++;
+        }
+      }
+    }
+  },
+  (timeMs) => console.log(`\t'fastbitset' ran in ${timeMs.toFixed(1)} ms`)
+);
 
 bitstrings = JSON.parse(
   fs.readFileSync(
@@ -30,13 +49,16 @@ bitstrings = JSON.parse(
   )
 ) as string[];
 
-bitsets = bitstrings.map((bitstring) => new BitSet(bitstring));
+bitsets_alexharri = bitstrings.map(
+  (bitstring) => new BitSet_alexharri(bitstring)
+);
+bitsets_fastbitset = bitstrings.map((bitstring) => makeFastBitSet(bitstring));
 
 console.log("\nRunning '500-sparse-bitstrings' benchmark");
 profile(
   () => {
     for (let n = 0; n < 50; n++) {
-      for (const bitset of bitsets) {
+      for (const bitset of bitsets_alexharri) {
         let count = 0;
         for (const _ of bitset) {
           count++;
@@ -45,4 +67,17 @@ profile(
     }
   },
   (timeMs) => console.log(`\t'alexharri/bitset' ran in ${timeMs.toFixed(1)} ms`)
+);
+profile(
+  () => {
+    for (let n = 0; n < 50; n++) {
+      for (const bitset of bitsets_fastbitset) {
+        let count = 0;
+        for (const _ of bitset as any) {
+          count++;
+        }
+      }
+    }
+  },
+  (timeMs) => console.log(`\t'fastbitset' ran in ${timeMs.toFixed(1)} ms`)
 );
