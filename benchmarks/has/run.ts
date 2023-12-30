dedd853307b62d97544e9a9d771b92a7df826404
alexharri <alexharri2919@gmail.com>
2023-12-30 11:05:31 +0000

    perf: rename get benchmark to has, add set to comparison

--- benchmarks/has/run.ts
+++ benchmarks/has/run.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
-import { BitSet } from "../../src/bitset";
-import BitSet2 from "bitset";
+import { BitSet as BitSet_alexharri } from "../../src/bitset";
+import BitSet_bitset from "bitset";
 import { profile } from "../profile";
 
 let arrs = JSON.parse(
@@ -12,16 +12,29 @@ let arrs = JSON.parse(
 ) as [string, number[]][][];
 
 const bitsetsA = arrs.map((arr) =>
-  arr.map(([bitstring, indices]) => [new BitSet2(bitstring), indices] as const)
+  arr.map(
+    ([bitstring, indices]) => [new BitSet_bitset(bitstring), indices] as const
+  )
 );
 const bitsetsB = arrs.map((arr) =>
-  arr.map(([bitstring, indices]) => [new BitSet(bitstring), indices] as const)
+  arr.map(
+    ([bitstring, indices]) =>
+      [new BitSet_alexharri(bitstring), indices] as const
+  )
 );
+const sets = arrs.map((arr) =>
+  arr.map(
+    ([bitstring, indices]) =>
+      [new Set(new BitSet_alexharri(bitstring)), indices] as const
+  )
+);
+
+const N = 10;
 
 console.log("Running '10-times-1k-bitstring-and-indices' benchmark");
 profile(
   () => {
-    for (let n = 0; n < 10; n++) {
+    for (let n = 0; n < N; n++) {
       for (const arr of bitsetsA) {
         for (let i = 0; i < arr.length; i++) {
           const bitset = arr[i][0];
@@ -37,17 +50,33 @@ profile(
 );
 profile(
   () => {
-    for (let n = 0; n < 10; n++) {
+    for (let n = 0; n < N; n++) {
       for (const arr of bitsetsB) {
         for (let i = 0; i < arr.length; i++) {
           const bitset = arr[i][0];
           const indices = arr[i][1];
           for (const index of indices) {
-            bitset.get(index);
+            bitset.has(index);
           }
         }
       }
     }
   },
   (timeMs) => console.log(`\t'alexharri/bitset' ran in ${timeMs.toFixed(1)} ms`)
+);
+profile(
+  () => {
+    for (let n = 0; n < N; n++) {
+      for (const arr of sets) {
+        for (let i = 0; i < arr.length; i++) {
+          const set = arr[i][0];
+          const indices = arr[i][1];
+          for (const index of indices) {
+            set.has(index);
+          }
+        }
+      }
+    }
+  },
+  (timeMs) => console.log(`\t'set' ran in ${timeMs.toFixed(1)} ms`)
 );
